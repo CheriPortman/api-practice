08d45603b1cd53a1fdb0279ede42063a8900c31a
Cheri Portman <cheriportman@hotmail.com>
2019-03-07 16:11:43 -0800

    renaming some variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import meals from '../data/meals.js';
 import renderMealCards from './meal-list-components.js';
-import loadFilter from './filter-component.js';
+import loadSearch from './search-component.js';
 
 renderMealCards(meals);
-loadFilter();
+loadSearch();
