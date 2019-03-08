c3b5d03c7c241143b96a3270802df50e64d57a1b
Cheri Portman <cheriportman@hotmail.com>
2019-03-07 17:49:39 -0800

    starting to deal with window.location.hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import meals from '../data/meals.js';
 import renderMealCards from './meal-list-components.js';
-import loadSearch from './search-component.js';
+import './search-component.js';
 
 renderMealCards(meals);
-loadSearch();
+// loadSearch();
--- src/search-component.js
+++ src/search-component.js
@@ -1,10 +1,20 @@
-const searchForm = document.getElementById('search-form');
+import { writeSearchToQuery } from './hash-query.js';
 
-export default function loadSearch() {
-    searchForm.addEventListener('submit', event => {
-        event.preventDefault();
-        const formData = new FormData(searchForm);
-        const mainIngredient = formData.get('main-ingredient');
-        console.log('clicked', mainIngredient);
-    });
-}
+const searchForm = document.getElementById('search-form');
+// export default function loadSearch() {
+//     searchForm.addEventListener('submit', event => {
+//         event.preventDefault();
+//         const formData = new FormData(searchForm);
+//         const mainIngredient = formData.get('main-ingredient');
+//         console.log('clicked', mainIngredient);
+//     });
+// }
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(searchForm);
+    const mainIngredient = formData.get('main--ingredient');
+    const existingQuery = window.location.hash.slice(1);
+    const newQuery = writeSearchToQuery(existingQuery, mainIngredient);
+    window.location.hash = newQuery;
+});
