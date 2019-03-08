f1bb496b8f0bc79940de0aca5518a01312206ddb
Cheri Portman <cheriportman@hotmail.com>
2019-03-08 11:01:03 -0800

    search updates window.location.hash

--- src/search-component.js
+++ src/search-component.js
@@ -13,8 +13,9 @@ searchForm.addEventListener('submit', event => {
     event.preventDefault();
 
     const formData = new FormData(searchForm);
-    const mainIngredient = formData.get('main--ingredient');
+    const mainIngredient = formData.get('main-ingredient');
     const existingQuery = window.location.hash.slice(1);
     const newQuery = writeSearchToQuery(existingQuery, mainIngredient);
     window.location.hash = newQuery;
+    console.log('newQuery', newQuery);
 });
