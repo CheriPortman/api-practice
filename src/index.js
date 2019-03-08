85364fbfb2f9cd9c29488754db29407957220a63
Cheri Portman <cheriportman@hotmail.com>
2019-03-08 11:41:06 -0800

    eventList. in index.js logs changes in hash

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import meals from '../data/meals.js';
 import renderMealCards from './meal-list-components.js';
 import './search-component.js';
 import './paging-component.js';
+import { readFromQuery } from './hash-query.js';
 
 renderMealCards(meals);
-// loadSearch();
+//add event listener to hashchange in url 
+window.addEventListener('hashchange', () => {
+    const existingQuery = window.location.hash.slice(1);
+    const queryOptions = readFromQuery(existingQuery);
+    console.log(queryOptions);
+});
