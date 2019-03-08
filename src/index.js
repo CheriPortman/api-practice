a5947d166a71ae6f44cd88f448f82a51165a80b9
Cheri Portman <cheriportman@hotmail.com>
2019-03-08 14:10:44 -0800

    meals render based on main ingredient input

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import meals from '../data/meals.js';
+// import meals from '../data/meals.js';
 import renderMealCards from './meal-list-components.js';
 import './search-component.js';
 import { updateMainIngredient } from './search-component.js';
 import './paging-component.js';
 import { readFromQuery } from './hash-query.js';
+import makeSearchMealUrl from './make-search-meal-url.js';
 
-renderMealCards(meals);
+// renderMealCards(meals);
+
 //add event listener to hashchange in url 
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const queryOptions = readFromQuery(existingQuery);
     updateMainIngredient(queryOptions.searchTerm);
     
-    console.log(queryOptions);
+    const url = makeSearchMealUrl(queryOptions);
+    fetch(url)
+        .then(response => response.json())
+        .then(result => result.meals)
+        .then(meals => {
+            renderMealCards(meals);
+        });
+    console.log('index.js, queryOptions:', queryOptions);
 });
