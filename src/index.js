391ac9572ceb319a11ec0e1ef55106652ac97e13
Cheri Portman <cheriportman@hotmail.com>
2019-03-08 11:36:55 -0800

    hash updates w/page # using next/previous buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import meals from '../data/meals.js';
 import renderMealCards from './meal-list-components.js';
 import './search-component.js';
+import './paging-component.js';
 
 renderMealCards(meals);
 // loadSearch();
--- src/search-component.js
+++ src/search-component.js
@@ -1,14 +1,7 @@
 import { writeSearchToQuery } from './hash-query.js';
 
 const searchForm = document.getElementById('search-form');
-// export default function loadSearch() {
-//     searchForm.addEventListener('submit', event => {
-//         event.preventDefault();
-//         const formData = new FormData(searchForm);
-//         const mainIngredient = formData.get('main-ingredient');
-//         console.log('clicked', mainIngredient);
-//     });
-// }
+
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
 
