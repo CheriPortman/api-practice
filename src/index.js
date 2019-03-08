c338b5e35db220583fa077768b660e74ba48697a
Cheri Portman <cheriportman@hotmail.com>
2019-03-07 16:03:34 -0800

    filter input field registers click, inputted data console.logs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import meals from '../data/meals.js';
 import renderMealCards from './meal-list-components.js';
+import loadFilter from './filter-component.js';
 
 renderMealCards(meals);
+loadFilter();
